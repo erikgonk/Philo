1aa156a9ca852dc3baa63553c11e7c6cad39ee1f
erikgonk <twire01@gmail.com>
2024-09-13 14:07:58 +0200

    [check mistakes]

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 13:54:03 by erigonza          #+#    #+#             */
-/*   Updated: 2024/09/13 13:42:51 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:07:33 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@ int	ft_usleep(t_philo *p, long long int to_sleep)
  
 	tmp = 0;
 	time = ft_get_moment_time(p);
+	if (!((time + tmp) < p->data->time && tmp < to_sleep))
+	{
+		p->data->d_flag = 1;
+	}
 	while ((time + tmp) < p->data->time && tmp < to_sleep && p->data->d_flag == 0)
 	{
 		pthread_mutex_lock(&p->check_dead);
-//		p->data->d_flag = 1;
 		usleep(1000);
 		tmp += 1;
 		pthread_mutex_unlock(&p->check_dead);
 	}
-	pthread_mutex_lock(&p->check_dead);
-	pthread_mutex_unlock(&p->check_dead);
 	return (0);
 // check if it dies while doing the usleep
 // if no: do the usleep and rutrn 0
@@ -57,7 +58,7 @@ void	ft_eat(t_philo *p)
 		ft_usleep(p, p->data->eat);
 		pthread_mutex_unlock(p->fork2);
 	}
-	else
+	else if (p->data->d_flag == 0)
 		ft_usleep(p, p->data->eat);
 	pthread_mutex_unlock(&p->fork1);
 }
@@ -71,21 +72,19 @@ void	*ft_routine(void *data)
 	pthread_mutex_unlock(&p->check_dead);
 	while (p->data->d_flag == 0)
 	{
-		if (p->id % 2 != 0)
+		if (p->id % 2 != 0 && p->data->d_flag == 0)
 			ft_usleep(p, (p->data->eat - 1));
 		ft_eat(p);
-		if (p->times_eat < 0)
+		if (p->times_eat >= 0 && p->times_eat < 0)
 		{
+		printf("ACT\n");
 			p->t_end = 1;
 			break ;
 		}
 // check unnecesary locks
-		pthread_mutex_lock(p->fork2);
 		ft_print_action(p, ACT_SLEEP);
-		ft_usleep(p, p->data->sleep * 1000);
-		pthread_mutex_unlock(p->fork2);
-		pthread_mutex_lock(p->fork2);
+		if (p->data->d_flag == 0)
+			ft_usleep(p, p->data->sleep);
 		ft_print_action(p, ACT_THINK);
-		pthread_mutex_unlock(p->fork2);
 	}
 }
